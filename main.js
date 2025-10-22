d2895ccdd89d1ac4b914ced14f6bad3701ca4621
abdumajid-501 <dasturchi501@gmail.com>
2025-10-22 15:47:11 +0500

    have done

---
+++ main.js
@@ -0,0 +1,29 @@
+const showBtn = document.getElementById("show-btn");
+const modalEl = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+const overlayEl = document.getElementById("overlay");
+
+
+const addHidden = () => {
+  modalEl.classList.add("hidden");
+  overlayEl.classList.add("hidden");
+};
+
+showBtn.addEventListener("click", () => {
+  modalEl.classList.remove("hidden");
+  overlayEl.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  addHidden();
+});
+
+overlayEl.addEventListener("click", () => {
+  addHidden();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    addHidden();
+  }
+});
